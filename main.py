5f1c0a142d5fa752891433be52200fc80df92d86
Lluc Bulto <lluc.bulto@gmail.com>
2019-02-19 19:32:51 +0100

    asfdf

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-def dec_returns(func):
-    def wrapper(*args, **kwargs):
-        print("I do something before")
-        aux_out = func(*args, **kwargs)
-        print("I do something after")
-        return aux_out
-    return wrapper
-
-
-@dec_returns
-def los_hahas():
-    return "LOL"
-
-
-a = los_hahas()
-print(a)
+print ("Hello Worlasdasd")
