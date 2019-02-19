3184956f30d17aa07522252f1a9b37792b602e46
Lluc Bulto <lluc.bulto@gmail.com>
2019-02-19 19:25:49 +0100

    Commit branch test

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print ("Hello Worlasdasd")
+def dec_returns(func):
+    def wrapper(*args, **kwargs):
+        print("I do something before")
+        aux_out = func(*args, **kwargs)
+        print("I do something after")
+        return aux_out
+    return wrapper
+
+
+@dec_returns
+def los_hahas():
+    return "LOL"
+
+
+a = los_hahas()
+print(a)
